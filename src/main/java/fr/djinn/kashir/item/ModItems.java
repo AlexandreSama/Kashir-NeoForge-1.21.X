e207913be2489289337864bec3f97f533698ff19
AlexandreSama <alexandrehainy12@gmail.com>
2024-10-27 19:07:55 +0100

    Add my first(s) item(s) ! And the traduction with textures and models

---
+++ src/main/java/fr/djinn/kashir/item/ModItems.java
@@ -0,0 +1,20 @@
+package fr.djinn.kashir.item;
+
+import fr.djinn.kashir.Kashir;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Kashir.MODID);
+
+    public static final DeferredItem<Item> DEMONIUM = ITEMS.register("demonium",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_DEMONIUM = ITEMS.register("raw_demonium",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
